f4d437beabcaf722345e67887635cb55a3881d4a
JAFFUL <marthanaaokaileyafful@gmail.com>
2025-09-25 10:38:11 +0000

    Computing the factorial

---
+++ src/Factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter number you want to find factorial of: ");
+        int userInput = input.nextInt();
+
+        // Factorials end at 1
+        int result = 1;
+        while(userInput > 1){
+            result = result * userInput;
+
+            userInput --;
+        }
+
+        System.out.println("Result for " + userInput +" is " + result);
+    }
+}
